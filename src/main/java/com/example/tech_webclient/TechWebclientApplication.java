541a9a2fa80fb05ccde3ecedc1bb9a79d13ecf6c
이준희 <qzqz08266@bodycodi.com>
2022-09-16 16:06:31 +0900

    Initial commit

---
+++ src/main/java/com/example/tech_webclient/TechWebclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.tech_webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechWebclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechWebclientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tech_webclient/TechWebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tech_webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechWebclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
